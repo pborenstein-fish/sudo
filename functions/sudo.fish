ccea796a7a104e17696098246e201e1942ea8cf5
Philip Borenstein <pborenstein@gmail.com>
2019-05-05 09:21:09 -0400

    add sudo

---
+++ functions/sudo.fish
@@ -0,0 +1,8 @@
+# see https://github.com/fish-shell/fish-shell/issues/288#issuecomment-22762823
+function sudo
+    if test "$argv" = !!
+        eval command sudo $history[1]
+    else
+        command sudo $argv
+    end
+end
